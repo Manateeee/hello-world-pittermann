f147073d6fd7a1f1ed4f1fdc33197261568ae841
Manateeee <54984968+Manateeee@users.noreply.github.com>
2019-09-13 12:29:23 +0200

    Hello World zwei

--- HelloWorld2/HelloWorld2/Program.cs
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace HelloWorld2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Hello World!!!!");
+            Console.ReadLine();
         }
     }
 }
